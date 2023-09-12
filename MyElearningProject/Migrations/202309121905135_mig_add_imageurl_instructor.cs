6db459997160d5ca36dc2b1c5e54559bf520b524
Enes Ertaş <ertaseness34@gmail.com>
2023-09-12 22:48:14 +0300

    Add project files.

---
+++ MyElearningProject/Migrations/202309121905135_mig_add_imageurl_instructor.cs
@@ -0,0 +1,18 @@
+﻿namespace MyElearningProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_add_imageurl_instructor : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Instructors", "ImageUrl", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Instructors", "ImageUrl");
+        }
+    }
+}
